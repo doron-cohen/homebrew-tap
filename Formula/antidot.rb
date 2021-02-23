465c46e9085a9a489d5106ab18fd83ad37e1bfed
goreleaserbot <goreleaser@doron.dev>
2021-02-23 14:05:36 +0200

    Brew formula update for antidot version v0.6.2

--- Formula/antidot.rb
+++ Formula/antidot.rb
@@ -5,16 +5,16 @@
 class Antidot < Formula
   desc "Cleans up your $HOME from those pesky dotfiles"
   homepage "https://github.com/doron-cohen/antidot"
-  version "0.6.1"
+  version "0.6.2"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/doron-cohen/antidot/releases/download/v0.6.1/antidot_0.6.1_Darwin_x86_64.tar.gz"
-    sha256 "f8d4dc53dbe3d903e1c00910d3a4e49034dcf0dc7d0cd85107e0626734b278ae"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/doron-cohen/antidot/releases/download/v0.6.2/antidot_0.6.2_Darwin_x86_64.tar.gz"
+    sha256 "bef0ed69fa1565b8b943a51cbe90be74d71497798fff56049c8ea66eb909e8da"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/doron-cohen/antidot/releases/download/v0.6.1/antidot_0.6.1_Linux_x86_64.tar.gz"
-    sha256 "a59ef94837e338a5280dcec7a0b6aa6b662c77f58c23af7c363d35506a0b9811"
+    url "https://github.com/doron-cohen/antidot/releases/download/v0.6.2/antidot_0.6.2_Linux_x86_64.tar.gz"
+    sha256 "3027e8a1f79741c5b644cf00e52aa232864ca76a8f813337eab343bc123605ed"
   end
 
   def install
